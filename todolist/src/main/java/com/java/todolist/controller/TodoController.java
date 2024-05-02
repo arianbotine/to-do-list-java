c173475de782922c07cb43d53c83625400ff2977
Arian Botine <arian.botine@shift.com.br>
2024-05-02 18:52:11 -0300

    Criaçaõ do controller para expor a aplicação na web

---
+++ todolist/src/main/java/com/java/todolist/controller/TodoController.java
@@ -0,0 +1,36 @@
+package com.java.todolist.controller;
+
+import com.java.todolist.entity.Todo;
+import com.java.todolist.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    @Autowired
+    TodoService todoService;
+
+    @PostMapping
+    public List<Todo> create(@RequestBody Todo todo) {
+        return todoService.create(todo);
+    }
+
+    @GetMapping
+    public List<Todo> list() {
+        return todoService.list();
+    }
+
+    @PutMapping
+    public List<Todo> update(@RequestBody Todo todo) {
+        return todoService.update(todo);
+    }
+
+    @DeleteMapping("{id}")
+    public List<Todo> delete(@PathVariable("id") Long id) {
+        return todoService.delete(id);
+    }
+}
